43372d7e75aea81e46befe2c87edc30086463d2d
kadertosun <https://github.com/kadertosun>
2021-05-22 19:48:17 +0300

    Uye kayıt formu

---
+++ UyeForm/UyeForm/Controllers/UyeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UyeForm.Models;
+namespace UyeForm.Controllers
+{
+    public class UyeController : Controller
+    {
+        // GET: Uye
+        FormEntities Db = new FormEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(UyeDb veri)
+        {
+            Db.UyeDb.Add(veri);
+            Db.SaveChanges();
+            return View();
+        }
+    }
+}
